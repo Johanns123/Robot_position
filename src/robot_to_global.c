19b55b2b8d06d2ca0f87539a6accd8880754e1ec
Johann Steffanno Jerônimo da Costa Caniato Amorim <johannsamorim@gmail.com>
2024-03-03 01:16:21 -0300

    Simulator finished and working. Next Step implemnting a GUI

--- src/robot_to_global.c
+++ src/robot_to_global.c
@@ -6,6 +6,14 @@
 
 #define DEG_TO_RAD M_PI/180
 #define RAD_TO_DEG 180/M_PI
+#define LENGTH 0.146 //meters
+#define RADIUS 0.040 //meters
+#define LIMIT_LINEAR_PRECISION_RATE 0.001
+#define LIMIT_ANGULAR_PRECISION_RATE 0.001
+#define LIMIT_ANGULAR_ROBOT_SPEED 18.367123
+#define LIMIT_LINEAR_ROBOT_SPEED 1.34
+#define LIMIT_ANGULAR_MOTOR_SPEED 33.52
+#define LIMIT_OUTPUT_VOLTAGE 12
 
 typedef struct
 {
@@ -24,15 +32,32 @@ typedef struct
     float delta_theta;
 } coordinate_local;
 
-void calculate_coordinates (coordinate_global *global, coordinate_local *local, float distance);
+typedef struct
+{
+    float uk_ang;
+    float uk_ang1;
+    float uk_disp;
+    float uk_disp1;
+} external_controller;
 
-float theta_desired = 0, theta0 = 0, desired_displacement = 0;
-float d = 0, d0 = 0, T = 50e-3;
+
+void calculate_coordinates (coordinate_global *global, coordinate_local *local, float distance);
+external_controller control_system(uint8_t mode, float ang_setpoint, float PV_ang_setpoint, float lin_setpoint, float PV_lin_setpoint);
+void control_motors(float w_refR, float w_refL, float *yk_MR, float *yk_ML);
+
+float T = 50e-3;
 
 int main(void)
 {
+    
+    static uint16_t i = 0;
+    static float v = 0, w_robot =  0, DS; //meters
+    static float WR_ref = 0, WL_ref = 0, VR = 0, VL = 0, yk_MR, yk_ML;
+    // static float voltsR = 0, voltsL = 0; //for opne loop
+    float theta_desired = 0, desired_displacement = 0;
     coordinate_global global;
     coordinate_local robot;
+    external_controller ext_ctrl;
     global.theta_1 = 0, global.theta = 0;
     robot.theta_1 = 0, robot.theta = 0;
     // unsigned int v = 0;
@@ -63,8 +88,10 @@ int main(void)
         return 0;
     }
 
-    X_desired = 0;
-    Y_desired = 0.20;
+    printf("Enter the X desired coordinate >> ");
+    scanf("%f", &X_desired);
+    printf("Enter the y desired coordinate >> ");
+    scanf("%f", &Y_desired);
 
     desired_displacement = sqrt((X_desired*X_desired)+(Y_desired*Y_desired));
     theta_desired = atan2(Y_desired, X_desired);
@@ -75,16 +102,6 @@ int main(void)
 
     while(1)
     {
-        static float yk_MR = 0, yk_1_MR = 0, uk_1_MR = 0;  //u is voltage 
-        static float yk_ML = 0, yk_1_ML = 0, uk_1_ML = 0; 
-        static float uk_MR = 0, ek_MR = 0, ek_1_MR = 0;
-        static float uk_ML = 0, ek_ML = 0, ek_1_ML = 0;
-        static float w_ref_MR =  5, w_ref_ML = 5; //ang freq reference
-        static uint16_t i = 0;
-        static float v = 0, w_robot =  0, DS, radius = 0.04, L = 0.146; //meters
-        static float V_robot = 0, VR = 0, VL = 0;
-        static float uk = 0, uk_1, error_ang_k = 0, error_ang_k1 = 0;
-        static float voltsR = 0, voltsL = 0, error_disp_k, error_disp_k1, uk_disp, uk_disp_1;
         //calculate the actual global theta
         calculate_coordinates(&global, &robot, DS);
 
@@ -108,72 +125,26 @@ int main(void)
         i++;
 
         //calculating displacement and robot theta
-        v       = (radius * ((yk_MR) + (yk_ML)) / 2) * T;
+        v       = (RADIUS * ((yk_MR) + (yk_ML)) / 2) * T;
         DS     += v;
-        w_robot = (radius * ((yk_MR) - (yk_ML)) / L) * T;
+        w_robot = (RADIUS * ((yk_MR) - (yk_ML)) / LENGTH) * T;
         robot.theta  += w_robot;
 
         //system input
 
-        if(abs((desired_displacement - DS)) >= 0.001 && abs((theta_desired - global.theta)) >= 0.0001)
+        if(abs((desired_displacement - DS)) >= LIMIT_LINEAR_PRECISION_RATE && abs((theta_desired - global.theta)) >= LIMIT_ANGULAR_PRECISION_RATE)
         {            
-            error_ang_k = theta_desired - global.theta;
-            uk = 1.4939*error_ang_k - 1.442808*error_ang_k1 + uk_1;
-            if(uk > 18.367123)
-                uk  = 18.367123;
-            else if(uk < -18.367123)
-                uk = -18.367123;
-            // uk = theta_desired; //w
-
-            error_ang_k1 = error_ang_k;
-            uk_1 = uk;
-
-            error_disp_k = desired_displacement - DS;
-            uk_disp = 1.4939*error_disp_k - 1.442808*error_disp_k1 + uk_disp_1; //linear speed
-            if(uk_disp > 1.34)
-                uk_disp  = 1.34;
-            else if(uk_disp < -1.34)
-                uk_disp = -1.34;
-            error_disp_k1 = error_disp_k;
-            uk_disp_1 = uk_disp;
+            ext_ctrl = control_system(0, theta_desired, global.theta, desired_displacement, DS);
         }
 
-        else if(abs(desired_displacement - DS) >= 0.001 && abs(theta_desired - global.theta) <= 0.0001)
+        else if(abs(desired_displacement - DS) >= LIMIT_LINEAR_PRECISION_RATE && abs(theta_desired - global.theta) <= LIMIT_ANGULAR_PRECISION_RATE)
         {
-            uk = 0;
-
-            error_disp_k = desired_displacement - DS;
-           uk_disp = 1.4939*error_disp_k - 1.442808*error_disp_k1 + uk_disp_1; //linear speed
-            if(uk_disp > 1.34)
-                uk_disp  = 1.34;
-            else if(uk_disp < -1.34)
-                uk_disp = -1.34;
-            error_disp_k1 = error_disp_k;
-            uk_disp_1 = uk_disp;
-
-            // V_robot = 0.5*T; // m/s - definido        
-            // uk_disp = V_robot/T;
-
-            VR  = ((2*uk_disp) + (uk*L))/(2);
-            VL  = ((2*uk_disp) - (uk*L))/(2);
+            ext_ctrl = control_system(1, theta_desired, global.theta, desired_displacement, DS);
         }
 
-        else if(abs(desired_displacement - DS) <= 0.001 && abs(theta_desired - global.theta) >= 0.0001)
+        else if(abs(desired_displacement - DS) <= LIMIT_LINEAR_PRECISION_RATE && abs(theta_desired - global.theta) >= LIMIT_ANGULAR_PRECISION_RATE)
         {
-            error_ang_k = theta_desired - global.theta;
-            uk = 1.4939*error_ang_k - 1.442808*error_ang_k1 + uk_1;
-            if(uk > 18.367123)
-                uk  = 18.367123;
-            else if(uk < -18.367123)
-                uk = -18.367123;
-            // uk = theta_desired; //w
-
-            error_ang_k1 = error_ang_k;
-            uk_1 = uk;
-
-            uk_disp = 0;
-            VR  = ((2*uk_disp) + (uk*L))/(2);
-            VL  = ((2*uk_disp) - (uk*L))/(2);
+            ext_ctrl = control_system(2, theta_desired, global.theta, desired_displacement, DS);
         }
 
         else
@@ -181,96 +152,19 @@ int main(void)
             printf("(%4.2f) displacement (m) : %7.4f | desired disp (m) : %7.4f | global theta (o) : %7.4f\n", float(i*T), DS, desired_displacement, global.theta);
             break;
         }
+              
+        VR  = ((2*ext_ctrl.uk_disp) + (ext_ctrl.uk_ang*LENGTH))/(2);
+        VL  = ((2*ext_ctrl.uk_disp) - (ext_ctrl.uk_ang*LENGTH))/(2);
         
-        // V_robot = 0.5*T; // m/s - definido        
-        // uk_disp = V_robot/T;
-
-        VR  = ((2*uk_disp) + (uk*L))/(2);
-        VL  = ((2*uk_disp) - (uk*L))/(2);
-        
-
         //converting linear motor velocity to angular motor velocity
-        w_ref_MR = VR/radius;
-        w_ref_ML = VL/radius;
+        WR_ref = VR/RADIUS;
+        WL_ref = VL/RADIUS;
 
-        if(w_ref_MR > 33.52)
-            w_ref_MR = 33.52;
-        else if (w_ref_MR < -33.52)
-            w_ref_MR = -33.52;
-
-        if(w_ref_ML > 33.52)
-            w_ref_ML = 33.52;
-        else if (w_ref_ML < -33.52)
-            w_ref_ML = -33.52;
-
-        // voltsR = 0.347247*w_ref_MR;
-        // voltsL = 0.347247*w_ref_ML;
-
-        if(voltsR > 12)
-            voltsR = 12;
-        else if (voltsR < -12)
-            voltsR = -12;
-        
-        if(voltsL > 12)
-            voltsL = 12;
-        else if (voltsL < -12)
-            voltsL = -12;
-        
-        // w_ref_MR = -5;
-        // w_ref_ML = -10;
-
-        //motors control
-        ek_MR = w_ref_MR - yk_MR;
-        ek_ML = w_ref_ML - yk_ML;
-
-        // uk_MR = voltsR;
-        // uk_ML = voltsL;
-        uk_MR = 0.31167*ek_MR-0.109832*ek_1_MR+uk_1_MR;
-        uk_ML = 0.31167*ek_ML-0.109832*ek_1_ML+uk_1_ML;
-
-        if(uk_MR > 12)
-            uk_MR = 12;
-        else if (uk_MR < -12)
-            uk_MR = -12;
-        
-        if(uk_ML > 12)
-            uk_ML = 12;
-        else if (uk_ML < -12)
-            uk_ML = -12;
-
-        yk_MR = 0.5647*yk_1_MR + 1.387*uk_1_MR;
-        yk_ML = 0.5647*yk_1_ML + 1.387*uk_1_ML;
-
-        if(yk_MR > 33.52)
-            yk_MR = 33.52;
-        else if (yk_MR < -33.52)
-            yk_MR = -33.52;
-
-        if(yk_ML > 33.52)
-            yk_ML = 33.52;
-        else if (yk_ML < -33.52)
-            yk_ML = -33.52;
-        //updating past samples
-        yk_1_MR = yk_MR;
-        yk_1_ML = yk_ML; //rad/s
-        ek_1_MR = ek_MR;
-        ek_1_ML = ek_ML;
-        uk_1_MR = uk_MR;
-        uk_1_ML = uk_ML;
-
-        // else
-        // {
-        //     yk_MR = 0;
-        //     yk_ML = 0;
-        //     printf("(%7.2f) displacement (m) : %7.4f | desired disp (m) : %7.4f | linear speed (m/s) : %7.4f\n", float(i*T), DS, desired_displacement, (v*T));
-        //     break;
-        // }
+        control_motors(WR_ref, WL_ref, &yk_MR, &yk_ML);
 
         // usleep(50000);
         Sleep(50);
     }
-    
-
     return 0;
 }
 
@@ -291,3 +185,137 @@ void calculate_coordinates (coordinate_global *global, coordinate_local *local, 
     local->theta_1 = local->theta; 
 }
 
+external_controller control_system(uint8_t mode, float ang_setpoint, float PV_ang_setpoint, float lin_setpoint, float PV_lin_setpoint)
+{
+    static float error_ang_k = 0, error_ang_k1 = 0,
+    error_disp_k, error_disp_k1;
+    external_controller ang_disp;
+
+    if(mode == 0)
+    {
+        error_ang_k = ang_setpoint - PV_ang_setpoint;
+        ang_disp.uk_ang = 1.4939*error_ang_k - 1.442808*error_ang_k1 + ang_disp.uk_ang1;
+        if(ang_disp.uk_ang > LIMIT_ANGULAR_ROBOT_SPEED)
+            ang_disp.uk_ang  = LIMIT_ANGULAR_ROBOT_SPEED;
+        else if(ang_disp.uk_ang < -LIMIT_ANGULAR_ROBOT_SPEED)
+            ang_disp.uk_ang = -LIMIT_ANGULAR_ROBOT_SPEED;
+        // uk = 0.7845; //w //open loop
+
+        error_ang_k1 = error_ang_k;
+        ang_disp.uk_ang1 = ang_disp.uk_ang;
+
+        error_disp_k = lin_setpoint - PV_lin_setpoint;
+        ang_disp.uk_disp = 1.4939*error_disp_k - 1.442808*error_disp_k1 + ang_disp.uk_disp1; //linear speed
+        if(ang_disp.uk_disp > LIMIT_LINEAR_ROBOT_SPEED)
+            ang_disp.uk_disp  = LIMIT_LINEAR_ROBOT_SPEED;
+        else if(ang_disp.uk_disp < -LIMIT_LINEAR_ROBOT_SPEED)
+            ang_disp.uk_disp = -LIMIT_LINEAR_ROBOT_SPEED;
+        error_disp_k1 = error_disp_k;
+        ang_disp.uk_disp1 = ang_disp.uk_disp;
+    }
+
+    else if(mode == 1)
+    {
+        error_disp_k = lin_setpoint - PV_lin_setpoint;
+        ang_disp.uk_disp = 1.4939*error_disp_k - 1.442808*error_disp_k1 + ang_disp.uk_disp1; //linear speed
+        if(ang_disp.uk_disp > LIMIT_LINEAR_ROBOT_SPEED)
+            ang_disp.uk_disp  = LIMIT_LINEAR_ROBOT_SPEED;
+        else if(ang_disp.uk_disp < -LIMIT_LINEAR_ROBOT_SPEED)
+            ang_disp.uk_disp = -LIMIT_LINEAR_ROBOT_SPEED;
+        error_disp_k1 = error_disp_k;
+        ang_disp.uk_disp1 = ang_disp.uk_disp;
+    }
+
+    else
+    {
+        error_ang_k = ang_setpoint - PV_ang_setpoint;
+        ang_disp.uk_ang = 1.4939*error_ang_k - 1.442808*error_ang_k1 + ang_disp.uk_ang1;
+        if(ang_disp.uk_ang > LIMIT_ANGULAR_ROBOT_SPEED)
+            ang_disp.uk_ang  = LIMIT_ANGULAR_ROBOT_SPEED;
+        else if(ang_disp.uk_ang < -LIMIT_ANGULAR_ROBOT_SPEED)
+            ang_disp.uk_ang = -LIMIT_ANGULAR_ROBOT_SPEED;
+        // uk = 0.7845; //w //open loop
+
+        error_ang_k1 = error_ang_k;
+        ang_disp.uk_ang1 = ang_disp.uk_ang;
+
+        ang_disp.uk_disp = 0; //zero linear speed
+    }
+
+    // V_robot = 0.5*T; // m/s - definido        
+    // uk_disp = V_robot/T;
+
+    return ang_disp;
+}
+
+void control_motors(float w_ref_MR, float w_ref_ML, float *yk_MR, float *yk_ML)
+{
+    static float yk_1_MR = 0, uk_1_MR = 0;  //u is voltage 
+    static float yk_1_ML = 0, uk_1_ML = 0; 
+    static float uk_MR = 0, ek_MR = 0, ek_1_MR = 0;
+    static float uk_ML = 0, ek_ML = 0, ek_1_ML = 0;
+
+
+    if(w_ref_MR > LIMIT_ANGULAR_MOTOR_SPEED)
+            w_ref_MR = LIMIT_ANGULAR_MOTOR_SPEED;
+    else if (w_ref_MR < -LIMIT_ANGULAR_MOTOR_SPEED)
+        w_ref_MR = -LIMIT_ANGULAR_MOTOR_SPEED;
+
+    if(w_ref_ML > LIMIT_ANGULAR_MOTOR_SPEED)
+        w_ref_ML = LIMIT_ANGULAR_MOTOR_SPEED;
+    else if (w_ref_ML < -LIMIT_ANGULAR_MOTOR_SPEED)
+        w_ref_ML = -LIMIT_ANGULAR_MOTOR_SPEED;
+
+    // voltsR = 0.347247*w_ref_MR; //open loop
+    // voltsL = 0.347247*w_ref_ML;
+
+    // if(voltsR > 12)
+    //     voltsR = 12;
+    // else if (voltsR < -12)
+    //     voltsR = -12;
+    
+    // if(voltsL > 12)
+    //     voltsL = 12;
+    // else if (voltsL < -12)
+    //     voltsL = -12;
+
+    //motors control
+    ek_MR = w_ref_MR - *yk_MR;
+    ek_ML = w_ref_ML - *yk_ML;
+
+    // uk_MR = voltsR; //open loop
+    // uk_ML = voltsL;
+    uk_MR = 0.31167*ek_MR-0.109832*ek_1_MR+uk_1_MR;
+    uk_ML = 0.31167*ek_ML-0.109832*ek_1_ML+uk_1_ML;
+
+    if(uk_MR > LIMIT_OUTPUT_VOLTAGE)
+        uk_MR = LIMIT_OUTPUT_VOLTAGE;
+    else if (uk_MR < -LIMIT_OUTPUT_VOLTAGE)
+        uk_MR = -LIMIT_OUTPUT_VOLTAGE;
+    
+    if(uk_ML > LIMIT_OUTPUT_VOLTAGE)
+        uk_ML = LIMIT_OUTPUT_VOLTAGE;
+    else if (uk_ML < -LIMIT_OUTPUT_VOLTAGE)
+        uk_ML = -LIMIT_OUTPUT_VOLTAGE;
+
+    //motors plant
+    *yk_MR = 0.5647*yk_1_MR + 1.387*uk_1_MR;
+    *yk_ML = 0.5647*yk_1_ML + 1.387*uk_1_ML;
+
+    if(*yk_MR > LIMIT_ANGULAR_MOTOR_SPEED)
+        *yk_MR = LIMIT_ANGULAR_MOTOR_SPEED;
+    else if (*yk_MR < -LIMIT_ANGULAR_MOTOR_SPEED)
+        *yk_MR = -LIMIT_ANGULAR_MOTOR_SPEED;
+
+    if(*yk_ML > LIMIT_ANGULAR_MOTOR_SPEED)
+        *yk_ML = LIMIT_ANGULAR_MOTOR_SPEED;
+    else if (*yk_ML < -LIMIT_ANGULAR_MOTOR_SPEED)
+        *yk_ML = -LIMIT_ANGULAR_MOTOR_SPEED;
+    //updating past samples
+    yk_1_MR = *yk_MR;
+    yk_1_ML = *yk_ML; //rad/s
+    ek_1_MR = ek_MR;
+    ek_1_ML = ek_ML;
+    uk_1_MR = uk_MR;
+    uk_1_ML = uk_ML;
+}
